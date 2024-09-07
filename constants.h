1c7daf68a159c490ed00156745036da2bc27caa5
Jonathan ArchDell <jonathancardonaramirez@gmail.com>
2024-09-07 15:31:34 -0500

    Better integration to items spacing

--- constants.h
+++ constants.h
@@ -5,10 +5,10 @@ const int DISPLAY_WIDTH = 128;
 const int DISPLAY_HEIGHT = 32;
 const int ICON_WIDTH = 28;
 const int ICON_HEIGHT = 28;
-const int ITEM_WIDTH = 31;
+const int ICON_SPACING = 3;
+const int ITEM_WIDTH = ICON_WIDTH + ICON_SPACING;
 const int SCROLL_BAR_WIDTH = 7;
 const int SCROLL_BAR_HEIGHT = 3;
-const int ICON_SPACING = 3;
 const int VISIBLE_ICONS =
     DISPLAY_WIDTH / (ICON_SPACING + ICON_WIDTH); // in this case 4
 
