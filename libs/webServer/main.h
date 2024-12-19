14716121fcb9cc0328ab225bbf3e91d7ea85e72f
Jonathan ArchDell <jonathancardonaramirez@gmail.com>
2024-12-18 22:46:48 -0500

    Web server logic starting point

---
+++ libs/webServer/main.h
@@ -0,0 +1,25 @@
+#include <WebServer.h>
+#include <WiFi.h>
+
+#include "serverVars.h"
+#include "pages.h"
+
+void setupServerRoutes() {
+  // Configurar rutas del servidor
+  server.on("/", handleRoot);
+  server.on("/setBank", handleSetBank);
+  server.on("/editBank", handleEditBank); // Nueva ruta para editar banco
+  server.on("/saveBank", handleSaveBank); // Nueva ruta para guardar banco
+}
+
+void setupServer() {
+
+  // AP Mode
+  WiFi.softAP(ssid, password);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+
+  delay(100);
+
+  setupServerRoutes();
+  server.begin(); // Iniciar servidor web
+}
---
+++ libs/webServer/pages.h
@@ -0,0 +1,15 @@
+// Función para manejar la página principal
+void handleRoot() {
+  String html = "<html><html>";
+  server.send(200, "text/html", html);
+}
+
+// Función para manejar la configuración del banco actual
+void handleSetBank() {
+}
+
+void handleEditBank() {
+}
+
+void handleSaveBank() {
+}
